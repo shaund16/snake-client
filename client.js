982093448f1cf427e430b6a5761c2db5427a2f96
Shaun <s-purslow@hotmail.com>
2021-06-29 23:01:45 +0000

    add name to the server

--- client.js
+++ client.js
@@ -4,18 +4,21 @@ const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
     host: "10.0.2.15",
-    port: "50541"
+    port: 50541
   });
 
 
-  conn.on("connect", () => console.log('Successfully connected to the game server!'));
+  conn.on("connect", () => {
+    console.log('Successfully connected to the game server!');
+    conn.write('Name: SDP');
   
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
+    // interpret incoming data as text
+    conn.setEncoding("utf8");
+  });
   return conn;
 };
 
 
 module.exports = {connect};
+
 
