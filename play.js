81c069ecf3cdc6640098e5091aa937533b860e1a
Shaun <s-purslow@hotmail.com>
2021-06-30 02:46:13 +0000

    make a new file input and move functions from play into it

--- play.js
+++ play.js
@@ -1,20 +1,7 @@
 const {connect} = require('./client');
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function () {
-  // your code here
-};
-
+const { setupInput } = require('./input');
 
 console.log("Connecting ...");
 connect();
+
+setupInput();
